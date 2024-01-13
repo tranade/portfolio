11614e42daac9141f8afadd2b097baa43f57b81d
Tanvi Ranade <tanviranade@gmail.com>
2024-01-13 17:13:45 -0500

    added components for experience section

--- script.js
+++ script.js
@@ -11,6 +11,25 @@ function toggleMenu() {
 document.addEventListener("DOMContentLoaded", function() {
     animateTitle();
   });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     // Initial animation on page load
+//     animateTitle();
+
+//     // Add event listeners to the navigation links
+//     const navLinks = document.querySelectorAll('.nav-links a');
+
+//     navLinks.forEach(link => {
+//         link.addEventListener('click', function () {
+//             animateTitle();
+//             // // Wait for a short delay before animating the title
+//             // setTimeout(() => {
+//             //     animateTitle();
+//             // }, 500); // Adjust the timing based on your preference
+//         });
+//     });
+// });
+
   
   function animateTitle() {
     const titleElement = document.getElementById("animatedTitle");
@@ -24,5 +43,20 @@ document.addEventListener("DOMContentLoaded", function() {
         titleElement.innerText += char;
       }, index * 100); // Adjust the timing based on your preference
     });
+
+    // const titleElements = document.querySelectorAll(".title");
+
+    // titleElements.forEach(titleElement => {
+    //     const originalText = titleElement.innerText;
+    //     const characters = originalText.split("");
+
+    //     titleElement.innerText = "";
+
+    //     characters.forEach((char, index) => {
+    //         setTimeout(() => {
+    //             titleElement.innerText += char;
+    //         }, index * 100); // Adjust the timing based on your preference
+    //     });
+    // });
   }
   
