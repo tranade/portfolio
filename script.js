5bdaca1c18106047411162c5823ddf2543a84068
Tanvi Ranade <tanviranade@gmail.com>
2025-02-21 15:42:47 -0500

    update resume, skills, and styling

--- script.js
+++ script.js
@@ -5,32 +5,10 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
-
-// new
-
 document.addEventListener("DOMContentLoaded", function() {
     animateTitle();
   });
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     // Initial animation on page load
-//     animateTitle();
-
-//     // Add event listeners to the navigation links
-//     const navLinks = document.querySelectorAll('.nav-links a');
-
-//     navLinks.forEach(link => {
-//         link.addEventListener('click', function () {
-//             animateTitle();
-//             // // Wait for a short delay before animating the title
-//             // setTimeout(() => {
-//             //     animateTitle();
-//             // }, 500); // Adjust the timing based on your preference
-//         });
-//     });
-// });
-
-  
   function animateTitle() {
     const titleElement = document.getElementById("animatedTitle");
     const originalText = titleElement.innerText;
@@ -41,22 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
     characters.forEach((char, index) => {
       setTimeout(() => {
         titleElement.innerText += char;
-      }, index * 100); // Adjust the timing based on your preference
+      }, index * 100);
     });
-
-    // const titleElements = document.querySelectorAll(".title");
-
-    // titleElements.forEach(titleElement => {
-    //     const originalText = titleElement.innerText;
-    //     const characters = originalText.split("");
-
-    //     titleElement.innerText = "";
-
-    //     characters.forEach((char, index) => {
-    //         setTimeout(() => {
-    //             titleElement.innerText += char;
-    //         }, index * 100); // Adjust the timing based on your preference
-    //     });
-    // });
   }
   
